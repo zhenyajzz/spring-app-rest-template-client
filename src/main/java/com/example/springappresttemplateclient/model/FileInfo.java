6b6a7e44af02399a2026c571097733a22fe8582a
zhenyajzz <evgeniy_goncharov_98@mail.ru>
2023-02-10 04:44:55 +0300

    I started to use lombok annotation in my as a FileInfo and ResponseMessage

--- src/main/java/com/example/springappresttemplateclient/model/FileInfo.java
+++ src/main/java/com/example/springappresttemplateclient/model/FileInfo.java
@@ -1,27 +1,14 @@
 package com.example.springappresttemplateclient.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FileInfo {
-  private String name;
-  private String url;
+    private String name;
+    private String url;
 
-  public FileInfo(String name, String url) {
-    this.name = name;
-    this.url = url;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getUrl() {
-    return this.url;
-  }
-
-  public void setUrl(String url) {
-    this.url = url;
-  }
 }
--- src/main/java/com/example/springappresttemplateclient/model/ResponseMessage.java
+++ src/main/java/com/example/springappresttemplateclient/model/ResponseMessage.java
@@ -1,18 +1,15 @@
 package com.example.springappresttemplateclient.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseMessage {
   private String message;
 
-  public ResponseMessage(String message) {
-    this.message = message;
-  }
 
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
 
 }
